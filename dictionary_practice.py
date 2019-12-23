ab4e34f755e8dd8051e655eca3788fd392e6ff5b
Parkerbp <ben.p.parker12@gmail.com>
2019-12-22 20:52:37 -0600

    updates

---
+++ dictionary_practice.py
@@ -0,0 +1,12 @@
+# Create an empty dictionary
+animal = dict()
+# Add k/v pairs
+animal["name"] = "Kevin"
+animal["breed"] = "Bulldog"
+animal["age"] = 5
+
+print(animal)
+
+for (key, value) in animal.items():
+    print(f"{key}: {value}") linkin12
+    
--- planet_list.py
+++ planet_list.py
@@ -11,15 +11,23 @@ planet_list.append("Pluto")
 
 del planet_list[-1]
 
-rocky_planets = planet_list[0:3]
+rocky_planets = planet_list[0:4]
 
 print("rocky_planets", rocky_planets)
 
 print("planet list", planet_list)
 
-spacecraft = [
-    ("Cassini", "Saturn")
-    ("Viking", "Mars")
-    ("Ulysses", "Jupiter")
+spacecrafts = [
+    ("Cassini", "Saturn"),
+    ("Viking", "Mars"),
+    ("Ulysses", "Jupiter"),
     ("Dragonfly", "Titan")
 ]
+
+
+for planet in planet_list:
+    print(planet)
+    for spacecraft in spacecrafts:
+        if planet == spacecraft[1]:
+            print("-" + spacecraft[0])
+        
